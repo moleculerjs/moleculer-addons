abd6f682a0b1db6a2dca5e0db9a8d04e5b5238aa
Icebob <mereg.norbert@gmail.com>
2017-05-25 16:35:54 +0200

    add base actions

--- services/moleculer-mongoose/examples/simple/index.js
+++ services/moleculer-mongoose/examples/simple/index.js
@@ -1,8 +1,8 @@
 "use strict";
 
-let { ServiceBroker } 	= require("moleculer");
-let MongooseService		= require("../../index");
-let Post				= require("../models/posts");
+let { ServiceBroker } = require("moleculer");
+let MongooseService = require("../../index");
+let Post = require("../models/posts");
 
 // Create broker
 let broker = new ServiceBroker({
@@ -13,30 +13,78 @@ let broker = new ServiceBroker({
 // Load my service
 broker.createService(MongooseService, {
 	name: "posts",
+	collection: Post,
 	settings: {
-		db: "mongo://localhost",
-		collection: Post
+		// Connection string
+		db: "mongodb://localhost",
+
+		propertyFilter: "title content votes",
+
+		populates: {
+			"author": "persons.model"
+		}
+	},
+
+	afterConnected() {
+		console.log("afterConnected: Connected successfully");
 	}
 });
 
 // Start server
-broker.start().then(() => {
-
+broker.start().delay(500).then(() => {
+	let id;
 	Promise.resolve()
 		// Drop all posts
+		.then(() => console.log("\n--- DROP ---"))
 		.then(() => broker.call("posts.drop").then(console.log))
+
+		// Count of posts
+		.then(() => console.log("\n--- COUNT ---"))
+		.then(() => broker.call("posts.count").then(console.log))
+		
 		// List posts
+		.then(() => console.log("\n--- LIST ---"))
 		.then(() => broker.call("posts.list").then(console.log))
 		// Create new Posts
-		.then(() => broker.call("posts.create").then(console.log))
+		.then(() => console.log("\n--- CREATE ---"))
+		.then(() => broker.call("posts.create", { entity: { title: "Hello", content: "Post content" } })
+			.then(doc => {
+				id = doc._id;
+				console.log("Saved: ", doc);
+			})
+		)
 		// Get a post
-		.then(() => broker.call("posts.get").then(console.log))
+		.then(() => console.log("\n--- GET ---"))
+		.then(() => broker.call("posts.get", { id }).then(console.log))
 		// Update a posts
-		.then(() => broker.call("posts.update").then(console.log))
+		.then(() => console.log("\n--- UPDATE ---"))
+		.then(() => broker.call("posts.update", { 
+			id, 
+			update: { 
+				$set: { 
+					title: "Hello 2", 
+					content: "Post content 2" 
+				} 
+			} 
+		}).then(console.log))
+		
+		// Count of posts
+		.then(() => console.log("\n--- COUNT ---"))
+		.then(() => broker.call("posts.count").then(console.log))
+		
 		// Remove a post
-		.then(() => broker.call("posts.remove").then(console.log))
+		.then(() => console.log("\n--- REMOVE ---"))
+		.then(() => broker.call("posts.remove", { id }).then(console.log))
+
+		// Count of posts
+		.then(() => console.log("\n--- COUNT ---"))
+		.then(() => broker.call("posts.count").then(console.log))
 
 		// Error handling
-		.catch(console.error);
+		.catch(console.error)
+
+		// Stop
+		.then(() => broker.stop());
+
 
 });
--- services/moleculer-mongoose/src/index.js
+++ services/moleculer-mongoose/src/index.js
@@ -6,6 +6,9 @@
 
 "use strict";
 
+const _ 		= require("lodash");
+const mongoose 	= require("mongoose");
+
 module.exports = {
 
 	name: "mongoose",
@@ -14,15 +17,118 @@ module.exports = {
 	 * Default settings
 	 */
 	settings: {
+		// Connection string
+		db: null,
 
+		// Field list for search
+		searchFields: null,
+
+		// Property filter
+		propertyFilter: null,
+
+		// Auto populates
+		populates: null
 	},
 
 	/**
 	 * Actions
 	 */
 	actions: {
-		test(ctx) {
-			return "Hello " + (ctx.params.name || "Anonymous");
+		/**
+		 * 
+		 */
+		list: {
+			cache: {
+				keys: [ "limit", "offset", "sort", "search" ]
+			},
+			handler(ctx) {
+				const filter = {};
+
+				const query = this.collection.find(filter);
+				return this.applyFilters(query, ctx.params).lean().exec();
+			}
+		},
+
+		/**
+		 * 
+		 */
+		count: {
+			cache: {
+				keys: [ "search" ]
+			},
+			handler(ctx) {
+				const filter = {};
+				// TODO: search
+				return this.collection.where(filter).count();
+			}
+		},		
+
+		/**
+		 * 
+		 */
+		create: {
+			handler(ctx) {
+				return this.Promise.resolve(ctx)
+					.then(ctx => {
+						const item = new this.collection(ctx.params.entity);
+						return item.save();
+					})
+					.then(doc => this.toJSON(doc))
+					.then(json => this.popuplateModels(ctx, json))
+					.then(json => this.clearCache().then(() => json));
+			}
+		},
+
+		/**
+		 * 
+		 */
+		get: {
+			cache: {
+				keys: [ "id" ]
+			},
+			handler(ctx) {
+				return this.Promise.resolve(ctx)
+					.then(ctx => {
+						return this.collection.findById(ctx.params.id).lean().exec();
+					})
+					.then(doc => this.toJSON(doc))
+					.then(json => this.popuplateModels(ctx, json));
+			}
+		},
+
+		/**
+		 * 
+		 */
+		update: {
+			handler(ctx) {
+				return this.Promise.resolve(ctx)
+					.then(ctx => {
+						return this.collection.findByIdAndUpdate(ctx.params.id, ctx.params.update, { "new": true });
+					})
+					.then(doc => this.toJSON(doc))
+					.then(json => this.popuplateModels(ctx, json))
+					.then(json => this.clearCache().then(() => json));
+			}
+		},
+
+		/**
+		 * 
+		 */
+		remove: {
+			handler(ctx) {
+				return this.collection.findByIdAndRemove(ctx.params.id)
+					.then(() => this.clearCache());
+			}
+		},
+
+		/**
+		 * 
+		 */
+		drop: {
+			handler() {
+				return this.collection.remove({})
+					.then(() => this.clearCache());
+			}
 		}
 	},
 
@@ -30,27 +136,151 @@ module.exports = {
 	 * Methods
 	 */
 	methods: {
+		/**
+		 * 
+		 * 
+		 * @returns 
+		 */
+		connect() {
+			let uri, opts;
+			if (_.isObject(this.settings.db) && this.settings.db.uri != null) {
+				uri = this.settings.db.uri;
+				opts = this.settings.db.opts;
+			} else {
+				uri = this.settings.db;			
+			}
 
+			this.logger.debug(`Connecting to MongoDB (${uri})...`);
+			this.db = mongoose.connect(uri, opts).connection;
+
+			return this.db;
+		},
+
+		/**
+		 * 
+		 * 
+		 * @param {any} q 
+		 * @param {any} params 
+		 * @returns 
+		 */
+		applyFilters(q, params) {
+			if (params) {
+				if (_.isNumber(params.limit))
+					q.limit(params.limit);
+
+				if (_.isNumber(params.offset))
+					q.skip(params.offset);
+
+				if (_.isString(params.sort))
+					q.sort(params.sort.replace(/,/, " "));
+
+				// TODO `search` with `searchField`
+			}
+			return q;
+		},
+
+		/**
+		 * Clear cache entities
+		 * 
+		 */
+		clearCache() {
+			this.broker.emit("cache.clean", this.name + ".*");
+			return Promise.resolve();
+		},
+
+		/**
+		 * Convert the `docs` MongoDB model to JSON object.
+		 * With `propFilter` can be filter the properties
+		 * 
+		 * @param {MongoDocument} 	docs		MongoDB document(s)
+		 * @param {String|Array} 			propFilter	Filter properties of model. It is a space-separated `String` or an `Array`
+		 * @returns								Object|Array
+		 * 
+		 * @memberOf Service
+		 */
+		toJSON(docs, propFilter) {
+			let func = function (doc) {
+				let json = (doc.constructor && doc.constructor.name === "model") ? doc.toJSON() : doc;
+
+				if (propFilter != null)
+					return _.pick(json, propFilter);
+
+				return json;
+			};
+
+			if (propFilter == null) {
+				propFilter = this.settings.modelPropFilter;
+			}
+
+			if (_.isString(propFilter))
+				propFilter = propFilter.split(" ");
+
+			if (_.isArray(docs)) {
+				return docs.map(doc => func(doc, propFilter));
+			} else if (_.isObject(docs)) {
+				return func(docs);
+			}
+		},
+
+		popuplateModels(ctx, docs) {
+			return docs;
+		}
 	},
 
 	/**
 	 * Service created lifecycle event handler
 	 */
 	created() {
+		if (!this.schema.collection)
+			throw new Error("Missing `collection` definition!");
 
+		mongoose.Promise = this.Promise;
+		this.collection = this.schema.collection;
+
+		this.db = null;
 	},
 
 	/**
 	 * Service started lifecycle event handler
 	 */
 	started() {
+		this.connect();
 
+		if (this.db) {
+			this.db.on("error", function mongoConnectionError(err) {
+				if (err.message.code === "ETIMEDOUT") {
+					this.logger.warn("Mongo connection timeout!", err);
+					setTimeout(() => {
+						this.connect();
+					}, 1000);
+					return;
+				}
+
+				this.logger.error("Could not connect to MongoDB!", this.settings.db);
+				this.logger.error(err);
+
+			}.bind(this));
+
+			this.db.once("open", function mongoAfterOpen() {
+				this.logger.info("Connected to MongoDB.");
+
+				// Call an 'afterConnected' handler in schema
+				if (_.isFunction(this.schema.afterConnected)) 
+					this.schema.afterConnected.call(this);
+			}.bind(this));	
+
+			this.db.on("disconnected", function mongoDisconnected() {
+				this.logger.warn("Disconnected from MongoDB.");
+			}.bind(this));	
+		}	
 	},
 
 	/**
 	 * Service stopped lifecycle event handler
 	 */
 	stopped() {
-
+		if (this.db) {
+			this.db.close();
+		}
 	}
 };
