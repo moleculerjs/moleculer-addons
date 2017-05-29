043656a5217b927899277478669f18e4f812faa7
Icebob <mereg.norbert@gmail.com>
2017-05-29 16:12:55 +0200

    delete old mongoose module

--- services/moleculer-mongoose/index.js
+++
@@ -1,9 +0,0 @@
-/*
- * moleculer-mongoose
- * Copyright (c) 2017 Ice Services (https://github.com/ice-services/moleculer-addons)
- * MIT Licensed
- */
-
-"use strict";
-
-module.exports = require("./src");
