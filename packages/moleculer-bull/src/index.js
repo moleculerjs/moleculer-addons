811bdb299f1cb660377bf8499561dab7ebda693e
intech <ru31337@gmail.com>
2020-01-10 15:08:22 +0300

    Add metrics for bull

--- packages/moleculer-bull/src/index.js
+++ packages/moleculer-bull/src/index.js
@@ -6,8 +6,8 @@
 
 "use strict";
 
-let _ 		= require("lodash");
-let Queue 	= require("bull");
+let _ = require("lodash");
+let Queue = require("bull");
 
 module.exports = function createService(url, queueOpts) {
 
@@ -31,13 +31,12 @@ module.exports = function createService(url, queueOpts) {
 			 * @returns {Job}
 			 */
 			createJob(name, jobName, payload, opts) {
-				if(opts)
+				if (opts)
 					return this.getQueue(name).add(jobName, payload, opts);
+				else if (payload)
+					return this.getQueue(name).add(jobName, payload);
 				else
-					if (payload)
-						return this.getQueue(name).add(jobName, payload);
-					else
-						return this.getQueue(name).add(jobName);
+					return this.getQueue(name).add(jobName);
 			},
 
 			/**
@@ -51,15 +50,100 @@ module.exports = function createService(url, queueOpts) {
 					this.$queues[name] = Queue(name, url, queueOpts);
 				}
 				return this.$queues[name];
+			},
+
+			/**
+			 * Set metrics for every queue
+			 * @param name
+			 */
+			addMetrics(name) {
+				// Collect metrics
+				try {
+					const queue = this.getQueue(name);
+					setInterval(async () => {
+						const {completed, active, delayed, failed, waiting} = await queue.getJobCounts();
+						this.broker.metrics.set("bull.completed", completed);
+						this.broker.metrics.set("bull.active", active);
+						this.broker.metrics.set("bull.delayed", delayed);
+						this.broker.metrics.set("bull.failed", failed);
+						this.broker.metrics.set("bull.waiting", waiting);
+					}, 1000);
+
+					// Collect duration job time
+					queue.on("global:completed", async id => {
+						try {
+							const job = await this.$queues[name].getJob(id);
+							if (!job) {
+								this.logger.warn({job: id}, "unable to find job from id");
+								return;
+							}
+							if (!job.finishedOn) return;
+							const duration = job.finishedOn - job.processedOn;
+							console.log({duration});
+							this.broker.metrics.observe("bull.completed.duration", duration);
+						} catch (err) {
+							this.logger.error({err, job: id}, "unable to fetch completed job");
+						}
+					});
+				} catch(err) {
+					this.logger.warn("Unable to collect bull jobs metrics.", err);
+				}
 			}
 		},
 
 		created() {
 			this.$queues = {};
+			if(this.broker.metrics) {
+				this.logger.info("Metrics enabled");
+				this.broker.metrics.register({
+					type: "gauge",
+					name: "bull.completed",
+					labelNames: ["bull","queue"],
+					description: "Number of completed messages",
+					unit: "job"
+				});
+				this.broker.metrics.register({
+					type: "gauge",
+					name: "bull.active",
+					labelNames: ["bull","queue"],
+					description: "Number of active messages",
+					unit: "job"
+				});
+				this.broker.metrics.register({
+					type: "gauge",
+					name: "bull.delayed",
+					labelNames: ["bull","queue"],
+					description: "Number of delayed messages",
+					unit: "job"
+				});
+				this.broker.metrics.register({
+					type: "gauge",
+					name: "bull.failed",
+					labelNames: ["bull","queue"],
+					description: "Number of failed messages",
+					unit: "job"
+				});
+				this.broker.metrics.register({
+					type: "gauge",
+					name: "bull.waiting",
+					labelNames: ["bull","queue"],
+					description: "Number of waiting messages",
+					unit: "job"
+				});
+				this.broker.metrics.register({
+					type: "histogram",
+					name: "bull.completed.duration",
+					labelNames: ["bull","queue"],
+					description: "Time to complete jobs",
+					unit: "millisecond",
+					maxAgeSeconds: 600,
+					ageBuckets: 10
+				});
+			}
 		},
 
 		started() {
-			const setHandler = (handler, name)=>{
+			const setHandler = async (handler, name) => {
 				let args = [];
 
 				if (handler.name) {
@@ -70,23 +154,24 @@ module.exports = function createService(url, queueOpts) {
 					args.push(handler.concurrency);
 				}
 
-				args.push(handler.process.bind(this))
+				args.push(handler.process.bind(this));
 
-				this.getQueue(name).process(...args)
-			}
+				this.getQueue(name).process(...args);
+			};
 
 			if (this.schema.queues) {
-				_.forIn(this.schema.queues, (fn, name) => {
-					if(typeof fn === "function")
+				_.forIn(this.schema.queues, async (fn, name) => {
+					if(this.broker.metrics) this.addMetrics(name);
+					if (typeof fn === "function") {
 						this.getQueue(name).process(fn.bind(this));
-					else if(Array.isArray(fn)){
-						for(let handler of fn){
-							setHandler(handler, name)
+					} else if (Array.isArray(fn)) {
+						for (let handler of fn) {
+							setHandler(handler, name);
 						}
-					}else {
-						setHandler(fn, name)
+					} else {
+						setHandler(fn, name);
 					}
-				})
+				});
 			}
 
 			return this.Promise.resolve();
